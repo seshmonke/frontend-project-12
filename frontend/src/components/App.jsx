628e4bebb95ec3b8e69818281f7eee95b3c0e7fb
seshmonke <osk1sky@yandex.ru>
2025-02-07 15:18:26 +0300

    form redevelop

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -2,11 +2,21 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { NotFoundPage } from "./pages/NotFoundPage.jsx";
 import { LoginPage } from "./pages/LoginPage.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Navbar from "react-bootstrap/Navbar";
 
 const App = () => {
   return (
-    <div>
-        <nav></nav>
+    <Container className="bg-white h-100" fluid>
+      <Navbar className="bg-light-subtle shadow-sm">
+        <Container fluid>
+          <Navbar.Brand href="/">Sesh Chat</Navbar.Brand>
+        </Container>
+      </Navbar>
+
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LoginPage />} />
@@ -14,7 +24,7 @@ const App = () => {
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </BrowserRouter>
-    </div>
+    </Container>
   );
 };
 
--- frontend/src/components/pages/LoginPage.jsx
+++ frontend/src/components/pages/LoginPage.jsx
@@ -1,23 +1,33 @@
-import React from "react";
-import { redirect } from "react-router-dom";
+import React, { useState }  from "react";
+import { useNavigate } from "react-router-dom";
 import { Formik, Field, Form } from "formik";
 import loginImage from "../../assets/loginImage.png";
-import { Button, Image } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 import axios from "axios";
 
 const LoginForm = () => {
+  const navigate = useNavigate();
+  const [authError, setAuthError] = useState(null);
   const handleSubmit = async (values, { resetForm, isSubmitting }) => {
     try {
       console.log(values);
       const response = await axios.post("/api/v1/login", values);
       const { data } = response;
       resetForm();
-      window.localStorage.setItem(data.username, data.token);
-      redirect("/");
+      window.localStorage.setItem('userId', JSON.stringify(data));
+      setAuthError(null);
+      navigate("/");
       console.log("getItem", window.localStorage.getItem("admin"));
       console.log("response", response, "isSubmitting", isSubmitting);
     } catch (e) {
       console.log("error", e);
+      e.response ? setAuthError(e.response.data.message || "Ошибка авторизации") : setAuthError("Произошла ошибка. Попробуйте снова.");
+    } finally {
+      isSubmitting(false);
     }
   };
 
@@ -36,7 +46,7 @@ const LoginForm = () => {
             placeholder="Ваш ник"
             required
             id="username"
-            className="form-control"
+            className={`form-control ${authError && 'is-invalid'}`}
           />
           <label htmlFor="username">Ваш ник</label>
         </div>
@@ -48,7 +58,7 @@ const LoginForm = () => {
             placeholder="Пароль"
             required
             id="password"
-            className="form-control"
+            className={`form-control ${authError && 'is-invalid'}`}
           />
           <label htmlFor="password">Пароль</label>
         </div>
@@ -60,53 +70,37 @@ const LoginForm = () => {
   );
 };
 
+const FormCard = () => {
+  return (
+    <Card className="shadow-sm">
+      <Card.Body className="row">
+        <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
+          <img
+            src={loginImage}
+            alt="Войти"
+            className="rounded-circle img-fluid col-6"
+          />
+        </div>
+        <LoginForm />
+      </Card.Body>
+      <Card.Footer>
+        <span>Нет Аккаунта? </span>
+        <a href="/signup">Регистрация</a>
+      </Card.Footer>
+    </Card>
+  );
+};
+
 const LoginPage = () => {
   return (
-    <div>
-      <Image
-        src={ loginImage }
-        alt="Войти"
-        rounded-circle
-        fluid
-        className="w-25"
-      />
-
-      <LoginForm />
-
-      <span>Нет Аккаунта? </span>
-
-      <a href="/signup">Регистрация</a>
-    </div>
+    <Container fluid className="h-100">
+      <Row className="justify-content-center align-content-center h-100">
+        <Col className="col-12 col-md-8 col-xxl-6">
+          <FormCard />
+        </Col>
+      </Row>
+    </Container>
   );
-
-  /*
-  return (
-    <div className="container-fluid h-100">
-      <div className="row justify-content-center align-content-center h-100">
-        <div className="col-12 col-md-8 col-xxl-6">
-          <div className="card shadow-sm">
-            <div className="card-body row p-5">
-              <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
-                <img
-                  src={loginImage}
-                  alt="Войти"
-                  className="rounded-circle img-fluid smaller-image"
-                />
-              </div>
-              <LoginForm />
-            </div>
-            <div className="card-footer p-4">
-              <div className="text-center">
-                <span>Нет Аккаунта? </span>
-                <a href="/signup">Регистрация</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-  */
 };
 
 export { LoginPage };
