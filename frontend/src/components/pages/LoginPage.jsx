b7cc5589f44cb008d77d33f143d796d9411ed9b7
seshmonke <osk1sky@yandex.ru>
2025-03-26 20:15:55 +0300

    eslint fix

--- frontend/src/components/pages/LoginPage.jsx
+++ frontend/src/components/pages/LoginPage.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useRef, useState } from "react";
-import PropTypes from "prop-types";
-import { useNavigate } from "react-router-dom";
-import { Formik, Field, Form } from "formik";
-import loginImage from "../../assets/loginImage.png";
+import React, { useEffect, useRef, useState } from 'react';
+import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
+import { Formik, Field, Form } from 'formik';
+import loginImage from '../../assets/loginImage.png';
 import {
   Button,
   Container,
@@ -10,13 +10,13 @@ import {
   Col,
   Card,
   Form as BootstrapForm,
-} from "react-bootstrap";
-import axios from "axios";
-import { useDispatch } from "react-redux";
-import { setCredentials } from "../../slices/authSlice.js";
-import { useAuth } from "../../hooks/index.jsx";
-import { useTranslation } from "react-i18next";
-import { toast } from "react-toastify";
+} from 'react-bootstrap';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { setCredentials } from '../../slices/authSlice.js';
+import { useAuth } from '../../hooks/index.jsx';
+import { useTranslation } from 'react-i18next';
+import { toast } from 'react-toastify';
 import routes from '../../routes.js';
 
 const LoginForm = () => {
@@ -47,15 +47,15 @@ const LoginForm = () => {
       const response = await axios.post(routes.loginPath(), values);
       const { data } = response;
       resetForm();
-      window.localStorage.setItem("userId", JSON.stringify(data));
+      window.localStorage.setItem('userId', JSON.stringify(data));
       dispatch(setCredentials(data));
 
       logIn();
-      navigate("/");
-      console.log("getItem", window.localStorage.getItem("userId"));
-      console.log("response", response, "isSubmitting", isSubmitting);
+      navigate('/');
+      console.log('getItem', window.localStorage.getItem('userId'));
+      console.log('response', response, 'isSubmitting', isSubmitting);
     } catch (e) {
-      console.log("error", e);
+      console.log('error', e);
       //toast(e.message);
 
       e.response
@@ -66,40 +66,40 @@ const LoginForm = () => {
 
   return (
     <Formik
-      initialValues={{ username: "", password: "" }}
+      initialValues={{ username: '', password: '' }}
       onSubmit={handleSubmit}
     >
-      <Form className="col-12 col-md-6 mt-3 mt-mb-0">
-        <h1 className="text-center mb-4">{t("loginPage.title")}</h1>
-        <div className="form-floating mb-3">
+      <Form className='col-12 col-md-6 mt-3 mt-mb-0'>
+        <h1 className='text-center mb-4'>{t('loginPage.title')}</h1>
+        <div className='form-floating mb-3'>
           <Field
-            type="text"
-            name="username"
-            autocomplete="username"
-            placeholder="Ваш ник"
+            type='text'
+            name='username'
+            autocomplete='username'
+            placeholder='Ваш ник'
             required
-            id="username"
-            className={`form-control ${authError && "is-invalid"}`}
+            id='username'
+            className={`form-control ${authError && 'is-invalid'}`}
             innerRef={usernameRef}
           />
-          <label htmlFor="username">{t("loginPage.yourName")}</label>
+          <label htmlFor='username'>{t('loginPage.yourName')}</label>
         </div>
-        <div className="form-floating mb-3">
+        <div className='form-floating mb-3'>
           <Field
-            type="password"
-            name="password"
-            autocomplete="username"
-            placeholder="Пароль"
+            type='password'
+            name='password'
+            autocomplete='username'
+            placeholder='Пароль'
             required
-            id="password"
-            className={`form-control ${authError && "is-invalid"}`}
+            id='password'
+            className={`form-control ${authError && 'is-invalid'}`}
           />
-          <label htmlFor="password">{t("loginPage.yourPassword")}</label>
-          <BootstrapForm.Control.Feedback type="invalid">
+          <label htmlFor='password'>{t('loginPage.yourPassword')}</label>
+          <BootstrapForm.Control.Feedback type='invalid'>
             {authError}
           </BootstrapForm.Control.Feedback>
         </div>
-        <Button variant="primary" type="submit" className="w-100 mb-3">
+        <Button variant='primary' type='submit' className='w-100 mb-3'>
           Войти
         </Button>
       </Form>
@@ -110,23 +110,23 @@ const LoginForm = () => {
 const LoginPage = () => {
   const { t } = useTranslation();
   return (
-    <Container fluid className="h-100">
-      <Row className="justify-content-center align-content-center h-100">
-        <Col className="col-12 col-md-8 col-xxl-6">
-          <Card className="shadow-sm">
-            <Card.Body className="row">
-              <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
+    <Container fluid className='h-100'>
+      <Row className='justify-content-center align-content-center h-100'>
+        <Col className='col-12 col-md-8 col-xxl-6'>
+          <Card className='shadow-sm'>
+            <Card.Body className='row'>
+              <div className='col-12 col-md-6 d-flex align-items-center justify-content-center'>
                 <img
                   src={loginImage}
-                  alt="Войти"
-                  className="rounded-circle h-50"
+                  alt='Войти'
+                  className='rounded-circle h-50'
                 />
               </div>
               <LoginForm />
             </Card.Body>
             <Card.Footer>
-              <span>{t("loginPage.footer")}</span>
-              <a href="/signup">{t("loginPage.registration")}</a>
+              <span>{t('loginPage.footer')}</span>
+              <a href='/signup'>{t('loginPage.registration')}</a>
             </Card.Footer>
           </Card>
         </Col>
--- frontend/src/components/pages/SignUpPage.jsx
+++ frontend/src/components/pages/SignUpPage.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect, useState } from 'react';
 import {
   Container,
   Row,
@@ -7,17 +7,17 @@ import {
   Form,
   FloatingLabel,
   Button,
-} from "react-bootstrap";
-import * as Yup from "yup";
-import signUpImage from "../../assets/signUpImage.png";
-import { Formik, Field, Form as FormikForm } from "formik";
-import axios from "axios";
-import { useDispatch } from "react-redux";
-import { setCredentials } from "../../slices/authSlice";
-import { useAuth } from "../../hooks";
-import { useNavigate } from "react-router-dom";
-import { useTranslation } from "react-i18next";
-import { toast } from "react-toastify";
+} from 'react-bootstrap';
+import * as Yup from 'yup';
+import signUpImage from '../../assets/signUpImage.png';
+import { Formik, Field, Form as FormikForm } from 'formik';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { setCredentials } from '../../slices/authSlice';
+import { useAuth } from '../../hooks';
+import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { toast } from 'react-toastify';
 import routes from '../../routes.js';
 
 
@@ -53,55 +53,55 @@ const SignUpPage = () => {
         username,
         password,
       });
-      console.log("Создание нового пользователя респонс: ", response);
+      console.log('Создание нового пользователя респонс: ', response);
       resetForm();
-      window.localStorage.setItem("userId", JSON.stringify(response.data));
+      window.localStorage.setItem('userId', JSON.stringify(response.data));
       dispatch(setCredentials(response.data));
       setSignUpError(null);
       logIn();
-      navigate("/");
-      console.log("getItem", window.localStorage.getItem("userId"));
-      console.log("response", response, "isSubmitting", isSubmitting);
+      navigate('/');
+      console.log('getItem', window.localStorage.getItem('userId'));
+      console.log('response', response, 'isSubmitting', isSubmitting);
     } catch (error) {
-      console.log("error", error);
+      console.log('error', error);
       error.response?.status === 409
-        ? setSignUpError(t("notification.alreadyExist"))
-        : setSignUpError(t("notification.error"));
+        ? setSignUpError(t('notification.alreadyExist'))
+        : setSignUpError(t('notification.error'));
     }
   };
 
   useEffect(() => {
-    console.log("Асинхронное изменение состояния ошибки", signUpError);
+    console.log('Асинхронное изменение состояния ошибки', signUpError);
   }, [signUpError]);
 
   const SignupSchema = Yup.object().shape({
     username: Yup.string()
-      .required(t("validation.required"))
-      .min(3, t("validation.usernameMinMax"))
-      .max(20, t("validation.usernameMinMax")),
+      .required(t('validation.required'))
+      .min(3, t('validation.usernameMinMax'))
+      .max(20, t('validation.usernameMinMax')),
     password: Yup.string()
-      .required(t("validation.required"))
-      .min(6, t("validation.passwordMinAlt")),
+      .required(t('validation.required'))
+      .min(6, t('validation.passwordMinAlt')),
     confirmPassword: Yup.string()
-      .required(t("validation.required"))
-      .oneOf([Yup.ref("password"), null], t("validation.passwordMatch")),
+      .required(t('validation.required'))
+      .oneOf([Yup.ref('password'), null], t('validation.passwordMatch')),
   });
 
   const handleKeyDown = (event, submitForm, values, setFieldTouched) => {
-    if (event.key === "Enter") {
+    if (event.key === 'Enter') {
       event.preventDefault();
 
       // Определяем порядок полей
       const fieldOrder = Object.keys(fieldRefs);
-      console.log("fieldOrder", fieldOrder);
+      console.log('fieldOrder', fieldOrder);
 
       // Находим первое незаполненное поле
       const emptyField = fieldOrder.find(
-        (field) => values[field].trim() === ""
+        (field) => values[field].trim() === ''
       );
 
       if (emptyField) {
-        // Помечаем поле как "touched" для показа ошибки
+        // Помечаем поле как 'touched' для показа ошибки
         setFieldTouched(emptyField, true);
         // Устанавливаем фокус на первое незаполненное поле
         fieldRefs[emptyField].current?.focus();
@@ -113,34 +113,34 @@ const SignUpPage = () => {
   };
 
   return (
-    <Container fluid className="h-100">
-      <Row className="justify-content-center align-content-center h-100">
+    <Container fluid className='h-100'>
+      <Row className='justify-content-center align-content-center h-100'>
         <Col xs={12} md={8} xxl={6}>
-          <Card className="shadow-sm">
-            <Card.Body className="d-flex flex-column flex-md-row justify-content-around align-items-center p-5">
+          <Card className='shadow-sm'>
+            <Card.Body className='d-flex flex-column flex-md-row justify-content-around align-items-center p-5'>
               <div>
                 <img
                   src={signUpImage}
-                  className="rounded-circle h-50"
-                  alt="Регистрация"
+                  className='rounded-circle h-50'
+                  alt='Регистрация'
                 />
               </div>
               <Formik
                 onSubmit={handleSubmit}
                 initialValues={{
-                  username: "",
-                  password: "",
-                  confirmPassword: "",
+                  username: '',
+                  password: '',
+                  confirmPassword: '',
                 }}
                 validationSchema={SignupSchema}
               >
                 {({ isSubmitting, submitForm, values, setFieldTouched }) => (
-                  <Form as={FormikForm} className="w-50">
-                    <h1 className="text-center mb-4">
-                      {t("signUpPage.title")}
+                  <Form as={FormikForm} className='w-50'>
+                    <h1 className='text-center mb-4'>
+                      {t('signUpPage.title')}
                     </h1>
                     {signUpError && (
-                      <div className="alert alert-danger">{signUpError}</div>
+                      <div className='alert alert-danger'>{signUpError}</div>
                     )}
                     {Object.keys(fieldRefs).map((fieldName) => {
                       return (
@@ -148,13 +148,13 @@ const SignUpPage = () => {
                           key={fieldName}
                           controlId={fieldName}
                           label={
-                            fieldName === "username"
-                              ? t("signUpPage.username")
-                              : fieldName === "password"
-                              ? t("signUpPage.password")
-                              : t("signUpPage.confirmPassword")
+                            fieldName === 'username'
+                              ? t('signUpPage.username')
+                              : fieldName === 'password'
+                              ? t('signUpPage.password')
+                              : t('signUpPage.confirmPassword')
                           }
-                          className="mb-3"
+                          className='mb-3'
                         >
                           <Field name={fieldName}>
                             {({ field, meta }) => (
@@ -162,18 +162,18 @@ const SignUpPage = () => {
                                 <Form.Control
                                   {...field}
                                   type={
-                                    fieldName.includes("password")
-                                      ? "password"
-                                      : fieldName.includes("confirmPassword")
-                                      ? "password"
-                                      : "text"
+                                    fieldName.includes('password')
+                                      ? 'password'
+                                      : fieldName.includes('confirmPassword')
+                                      ? 'password'
+                                      : 'text'
                                   }
                                   placeholder={
-                                    fieldName === "username"
-                                      ? t("validation.usernameMinMax")
-                                      : fieldName === "password"
-                                      ? t("validation.passwordMin")
-                                      : t("validation.passwordMatch")
+                                    fieldName === 'username'
+                                      ? t('validation.usernameMinMax')
+                                      : fieldName === 'password'
+                                      ? t('validation.passwordMin')
+                                      : t('validation.passwordMatch')
                                   }
                                   isInvalid={meta.touched && !!meta.error}
                                   ref={fieldRefs[fieldName]}
@@ -186,7 +186,7 @@ const SignUpPage = () => {
                                     )
                                   }
                                 />
-                                <Form.Control.Feedback type="invalid" tooltip>
+                                <Form.Control.Feedback type='invalid' tooltip>
                                   {meta.touched && meta.error}
                                 </Form.Control.Feedback>
                               </>
@@ -196,13 +196,13 @@ const SignUpPage = () => {
                       );
                     })}
                     <Button
-                      variant="outline-primary"
-                      type="submit"
-                      className="w-100"
+                      variant='outline-primary'
+                      type='submit'
+                      className='w-100'
                     >
                       {isSubmitting
-                        ? t("signUpPage.sending")
-                        : t("signUpPage.registration")}
+                        ? t('signUpPage.sending')
+                        : t('signUpPage.registration')}
                     </Button>
                   </Form>
                 )}
