e2e2c4dbb7f295bb005f1943b48dca77b394cd21
seshmonke <osk1sky@yandex.ru>
2025-04-05 19:14:39 +0300

    svg fix

--- frontend/src/components/pages/MainPage.jsx
+++ frontend/src/components/pages/MainPage.jsx
@@ -24,19 +24,8 @@ import {
 import { setChannels, setCurrentChannel } from '../../slices/channelsSlice.js';
 import routes from '../../routes.js';
 import { useFilter } from '../../hooks/index.jsx';
-
-const MyIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 16 16"
-    width="20"
-    height="20"
-    fill="currentColor"
-  >
-    <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
-    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
-  </svg>
-);
+import addChannelButton from '../../assets/addChannelButton.svg';
+import sendMessageButton from '../../assets/sendMessageButton.svg';
 
 const Channels = ({ channels }) => {
   const { t } = useTranslation();
@@ -397,20 +386,7 @@ const MessageForm = () => {
           onChange={(e) => setInputValue(e.target.value)}
         />
         <button className="btn btn-group-vertical" type="submit">
-          <svg
-            type="submit"
-            disabled
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            width="20"
-            height="20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"
-            />
-          </svg>
+          <img src={sendMessageButton} alt="sendButtonMessage" width="20" height="20"/>
           <span className="visually-hidden">Отправить</span>
         </button>
       </BootstrapForm.Group>
@@ -428,6 +404,7 @@ const NewChannelButton = () => {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   const { auth, channels } = useSelector((state) => state);
+  const cleanWords = useFilter();
 
   const NewChannelSchema = Yup.object().shape({
     channelName: Yup.string()
@@ -453,7 +430,7 @@ const NewChannelButton = () => {
         className="p-0 text-primary btn btn-group-vertical"
         onClick={handleShow}
       >
-        <MyIcon />
+        <img src={addChannelButton} alt="addChannelButton" width="20" height="20" />
         <span className="visually-hidden">+</span>
       </button>
 
@@ -465,7 +442,7 @@ const NewChannelButton = () => {
           validationSchema={NewChannelSchema}
           onSubmit={async ({ channelName }) => {
             try {
-              const name = filter.clean(channelName);
+              const name = cleanWords(channelName);
               const response = await axios.post(
                 routes.channelsPath(),
                 { name },
